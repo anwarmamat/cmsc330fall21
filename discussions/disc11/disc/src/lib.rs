ff53ba7be7b0478252de6105bc5f7ceabea5f2de
Pranav Kulkarni <pranavkop@live.com>
2021-11-24 13:07:50 -0500

    Cleanup

--- discussions/disc11/disc/src/lib.rs
+++ discussions/disc11/disc/src/lib.rs
@@ -1,37 +1,20 @@
 /// Returns the sum of the even integers in the range [i, j).
 /// `sum_evens(0, 6) == 6`. (0 + 2 + 4)
 pub fn sum_evens(i: i32, j: i32) -> i32 {
-    let mut sum = 0;
-    for k in i..j {
-        if k % 2 == 0 {
-            sum += k;
-        }
-    }
-    sum
+    unimplemented!();
 }
 
 /// Returns the Euclidean distance between two 2-dimensional points.
 /// The points are represented as 2-tuples of [f64]s.
 /// `distance((0.0, 0.0), (1.0, 1.0) == 1.414...`
 pub fn distance((ax, ay): (f64, f64), (bx, by): (f64, f64)) -> f64 {
-    // sqrt((ax-bx)^2 + (ay-by)^2)
-    ((ax - bx).powi(2) + (ay - by).powi(2)).sqrt()
+    unimplemented!();
 }
-
-// i32.pow(i32)
-// f64.powi(i32), f64.powf(f64)
 
 /// Returns the sum of the squared elements of arr.
 /// `sum_squares(&[1, 2]) == 5 (1^2 + 2^2)`.
 pub fn sum_squares(arr: &[i32]) -> i32 {
-    let mut sum = 0;
-    // for i in 0..arr.len() {
-    //     sum += arr[i].pow(2)
-    // }
-    for i in arr.iter() {
-        sum += i.pow(2)
-    }
-    sum
+    unimplemented!();
 }
 
 /// Adds 1 to each element of the array. (Mutates the array.)
@@ -39,12 +22,7 @@ pub fn sum_squares(arr: &[i32]) -> i32 {
 /// `raise_1(&mut arr);`
 /// `arr == [1, 2, 3]`
 pub fn raise_1(arr: &mut [i32]) {
-    // for i in 0..arr.len() {
-    //     arr[i] = arr[i] + 1;
-    // }
-    for i in arr.iter_mut() {
-        *i = *i + 1
-    }
+    unimplemented!();
 }
 
 // CHALLENGE PROBLEM (UNGRADED)
